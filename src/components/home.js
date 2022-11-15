75773de1a44b108721ab29ada8fe4fd21e195ae6
Jyotsna574 <100153985+Jyotsna574@users.noreply.github.com>
2022-11-15 22:38:08 +0530

    flexcards

--- src/components/home.js
+++ src/components/home.js
@@ -5,14 +5,14 @@ function Home() {
     return (
       <body className='Body'>
           <div className='hello'>
-            <p> Hello, I’m</p>
+            <p> Hi, I’m</p>
           </div>
           <div className='vishnu'>
             <p>V I S H N U</p>
           </div>
           <div className='full-stack'>
             <p>
-            full-stack developer with 1 year experience designing, and developing applications and websites.
+            a MERN stack developer.
             </p>
           </div>
           </body>
--- src/components/works.js
+++ src/components/works.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import tasky from '../tasskk.webp'
+import pg from '../pg.jpeg'
+import g from '../github3d.jpg'
+import clock from '../clock3d.jpg'
 
 
 
@@ -8,12 +12,95 @@ function Works() {
       <div className='Works'>
           <div className='Myworks'> My works</div>
           <div className='work-cards'>
-            <div className='Frontend'> <img src="image.png"></img><h1> Frontend Development </h1> <h2>Proficient in html, css and javascript </h2></div>
-            <div className='backend'>  <img src="image.png"></img><h1> Backend Development </h1> <h2>Proficient in html, css and javascript </h2></div>
-            <div className='app'>  <img src="image.png"></img><h1> App Development </h1> <h2>Proficient in html, css and javascript </h2></div>
+             <h1> Frontend Applications
+             </h1>
+            
+            <div className='card1'>
+          <div className='container'>
+          <img src={tasky} ></img>
+          <div className='middle'>
+            <button className='tasky'>
+            TASKY</button>
+          </div>
+        </div>
+        </div>
+
+        <div className='card2'>
+        <div className='container'>
+          <img src={pg} ></img>
+          <div className='middle'>
+            <button className='pg'>
+            PASSWORD GENERATOR</button>
+          </div>
+        </div>
+        </div>
+        
+</div>
+<div className='work-cards'>
+           <h1> API based Wed Applications </h1>
+            <div className='container'>
+            <img src={tasky} ></img>
+            <div className='middle'>
+              <button className='tasky'>
+              TASKY</button>
             </div>
+          </div>
+  </div>
+
+  
+
+        
+
+  <div className='work-cards'>
+           <h1> MERN Stack Applications </h1>
+            <div className='container'>
+            <img src={tasky} ></img>
+            <div className='middle'>
+              <button className='tasky'>
+              TASKY</button>
+            </div>
+          </div>
+  </div>
+
+  <div className='work-cards'>
+             <h1> Fun Applications </h1>
+            
+            <div className='card1'>
+          <div className='container'>
+          <img src={tasky} ></img>
+          <div className='middle'>
+            <button className='tasky'>
+            TASKY</button>
+          </div>
+        </div>
+        </div>
+
+        <div className='card2'>
+        <div className='container'>
+          <img src={pg} ></img>
+          <div className='middle'>
+            <button className='pg'>
+            PASSWORD GENERATOR</button>
+          </div>
+        </div>
+        </div>
+        </div>
+
+
+
+  
+          
           
         </div>
+        
+        
+
+
+
+           
+     
+           
+          
      );
     }
     
